bee78b5418cd0c1c90f0713e77a3371d82be8b19
jdcc <jclark@cyber.law.harvard.edu>
2014-08-06 16:56:21 -0400

    closes #9491

--- functions.php
+++ functions.php
@@ -48,3 +48,16 @@ function twentythirteen_entry_meta() {
 		);
 	}
 }
+
+function add_secure_video_options($html) {
+  if (is_ssl() && strpos($html, "<iframe" ) !== false) {
+    $search = array('src="http://www.youtu','src="http://youtu');
+    $replace = array('src="https://www.youtu','src="https://youtu');
+    $html = str_replace($search, $replace, $html);
+
+    return $html;
+  } else {
+    return $html;
+  }
+}
+add_filter('the_content', 'add_secure_video_options', 10);
